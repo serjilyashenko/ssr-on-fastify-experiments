9c691e34aed4b6f6a45eb9c807a26389aa8fb27c
Serge Ilyashenko <age_bookend_0j@icloud.com>
2022-09-10 12:05:44 +0200

    register static and add favicon

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 'use strict'
+import path from 'path';
+import { fileURLToPath } from "url"
 import Fastify from 'fastify'
+import fastifyStatic from '@fastify/static';
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export async function createServer({port = 3000}) {
   const fastify = Fastify({logger: true})
 
+  fastify.register(fastifyStatic, {root: path.join(__dirname, '../public')})
+
   fastify.get('/', async (request, reply) => {
-    return { hello: 'world' }
+    return "Fastify + React = 🦐"
   })
 
   try {
